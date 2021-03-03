d826115219643480b98fe9f41ed80cdb249dca4d
kudpig <shin.kudo.aa@gmail.com>
2021-03-03 22:43:42 +0900

    [set]set-outlet

--- ButtonAnimation/ViewController.swift
+++ ButtonAnimation/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var pulseButton: UIButton!
+    
+    @IBOutlet weak var FlushButton: UIButton!
+    
+    @IBOutlet weak var ShakeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    func setupButton() {
+        pulseButton.layer.cornerRadius = 8
+        FlushButton.layer.cornerRadius = 8
+        ShakeButton.layer.cornerRadius = 8
+    }
+    
 }
 
